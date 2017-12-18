3f28280ebc98559684707a60e2cebf89172a6aef
Andrew Johnson <ajj@byu.net>
2017-12-18 13:19:10 -0600

    Track clicks on click counter

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 $(document).ready(start);
 
+var clickCounter = 0;
+
 function start() {
   console.log('srced');
 
   $('#generate').on('click', function() {
     $('#target').append('<div>hi</div>');
+    clickCounter++;
+    console.log(clickCounter);
   });
 }
