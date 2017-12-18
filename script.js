8c7fe3b3cc74c33e024167ae7a2395dbee24ba5d
Andrew Johnson <ajj@byu.net>
2017-12-18 13:33:15 -0600

    Changed how styles are assigned; 'swap' toggles red to yellow

--- script.js
+++ script.js
@@ -6,13 +6,23 @@ function start() {
   console.log('srced');
 
   $('#generate').on('click', function() {
-    var newDiv = $('<div id="div-' + clickCounter + '"></div>');
+    var newDiv = $('<div class="red"></div>');
     newDiv.append('<p>' + clickCounter + '</p>');
-    newDiv.append('<button id="swap">Swap</button>');
-    newDiv.append('<button id="delete">Delete</button>');
+    newDiv.append('<button class="swap">Swap</button>');
+    newDiv.append('<button class="delete">Delete</button>');
     $('#target').append(newDiv);
     console.log(clickCounter);
 
     clickCounter++;
   });
+
+  // SUMMARY: Add listener for swap buttons to change color
+    $('#target').on('click', '.swap', function() {
+      var thisDiv = $(this).closest('div');
+      thisDiv.toggleClass('red');
+      thisDiv.toggleClass('yellow');
+    })
+  // END
+
+
 }
